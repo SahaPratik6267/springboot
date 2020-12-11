de585b6043f98fdbdf8124d61be910981a9126f7
Fahima Rahman <fahima.rahman@stud.uni-bamberg.de>
2020-12-11 19:07:52 +0100

    initialized code (java 8 chosen default no need to change)

---
+++ src/main/java/com/example/bevarage_service/BevarageServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.bevarage_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BevarageServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BevarageServiceApplication.class, args);
+	}
+
+}
