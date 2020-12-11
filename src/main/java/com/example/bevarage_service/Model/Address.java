707e27abaec0cbf390dea93a256142d797e10c5e
Albert Rozario <albert.rozario@stud.uni-bamberg.com>
2020-12-11 22:18:48 +0100

    Creted entity for all the Models

--- src/main/java/com/example/bevarage_service/Model/Address.java
+++ src/main/java/com/example/bevarage_service/Model/Address.java
@@ -1,4 +1,12 @@
 package com.example.bevarage_service.Model;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
+@Data
+@AllArgsConstructor
 public class Address {
+    private Long id;
+    private String street;
+    private String number;
+    private String postalCode;
 }
--- src/main/java/com/example/bevarage_service/Model/Crate.java
+++ src/main/java/com/example/bevarage_service/Model/Crate.java
@@ -6,5 +6,10 @@ import lombok.Data;
 @Data
 @AllArgsConstructor
 public class Crate {
-
+    private long id;
+    private  String name;
+    private String cratePic;
+    private int noOfBottles;
+    private double price;
+    private int cratesInStock;
 }
