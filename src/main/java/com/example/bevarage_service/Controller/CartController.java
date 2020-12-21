3e1f458690a87d9c0890d54aa9f86ef7bde9610a
Md. Nazrul Islam <md.-nazrul.islam@stud.uni-bamberg.de>
2020-12-21 17:06:03 +0100

    Session implemented, data is passing from beverage page to basket page through session

--- src/main/java/com/example/bevarage_service/Controller/CartController.java
+++ src/main/java/com/example/bevarage_service/Controller/CartController.java
@@ -6,13 +6,20 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.List;
+
 @Slf4j
 @Controller
 @RequestMapping(value = "/basket")
 public class CartController {
     @GetMapping
-    public String getBottles(Model model) {
+    public String getBottles(Model model, HttpSession session) {
 
+        List<String> cartItems = (List<String>) session.getAttribute("CartSession");
+//        System.out.println(cartItems);
+        model.addAttribute("cartSession", cartItems !=null? cartItems :new ArrayList<>());
         return "basket";
     }
 }
