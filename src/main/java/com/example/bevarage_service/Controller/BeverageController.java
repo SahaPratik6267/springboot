bb7144c994eb634549c3d0e4f90bd50caad24e77
Albert Rozario <albert.rozario@stud.uni-bamberg.com>
2020-12-23 23:15:06 +0100

    fixed validation for bottle input

--- src/main/java/com/example/bevarage_service/Controller/BeverageController.java
+++ src/main/java/com/example/bevarage_service/Controller/BeverageController.java
@@ -70,7 +70,7 @@ public class BeverageController {
         //model.addAttribute("Crates", this.crateList);
 
         model.addAttribute("bottle", bottle);
-        // model.addAttribute("bottles", this.bottleRepository.findAll());
+        model.addAttribute("bottles", this.bottleRepository.findAll());
 
 
         return "beverage";
@@ -82,10 +82,37 @@ public class BeverageController {
 
 
         if (errors.hasErrors()) {
-            model.addAttribute("Bottles", this.bottleRepository.findAll());
+            this.beverageslist.clear();
+            this.bottleList.clear();
+            this.crateList.clear();
+
+            this.beverageslist = this.bottleRepository.findAll();
+
+            for (Beverage b : this.beverageslist) {
+                if (b instanceof Bottle) {
+                    this.bottleList.add((Bottle) b);
+                } else if (b instanceof Crate) {
+                    this.crateList.add((Crate) b);
+                } else {
+                    //error
+                }
+
+            }
+            model.addAttribute("Bottles", this.bottleList);
+
+            model.addAttribute("Crates", this.crateList);
+
+
+            //model.addAttribute("Crates", this.crateList);
+
+            model.addAttribute("bottle", bottle);
+            model.addAttribute("bottles", this.bottleRepository.findAll());
+
+
             return "beverage";
+
         }
-        //bottle.setId("" + this.bottleRepository.count()+1);
+
         this.bottleRepository.save(bottle);
 
         return "redirect:/beverage";
