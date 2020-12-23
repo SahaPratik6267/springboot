f373853e0d012d033bdd693444ba14f60e749376
Md. Nazrul Islam <md.-nazrul.islam@stud.uni-bamberg.de>
2020-12-23 15:40:52 +0100

    session bottles & crate

--- src/main/java/com/example/bevarage_service/Controller/CartController.java
+++ src/main/java/com/example/bevarage_service/Controller/CartController.java
@@ -1,9 +1,6 @@
 package com.example.bevarage_service.Controller;
 
-import com.example.bevarage_service.Model.Address;
-import com.example.bevarage_service.Model.OrderItem;
-import com.example.bevarage_service.Model.Orders;
-import com.example.bevarage_service.Model.User;
+import com.example.bevarage_service.Model.*;
 import com.example.bevarage_service.repository.AddressRepository;
 import com.example.bevarage_service.repository.OrderItemRepository;
 import com.example.bevarage_service.repository.OrdersRepository;
@@ -15,6 +12,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpSession;
 import java.time.LocalDate;
@@ -37,47 +35,54 @@ public class CartController {
         this.addressRepo = addressRepo;
         this.userrepo = userrepo;
         this.ordersrepo = ordersrepo;
-
     }
 
     @GetMapping
     public String getBottles(Model model, HttpSession session) {
 
-         cartItems = (List<String>) session.getAttribute("CartSession");
-        System.out.println(cartItems);
-        model.addAttribute("cartSession", cartItems != null ? cartItems : new ArrayList<>());
-        return "basket";
-    }
-    @PostMapping
-    public String submitOrders(Model model){
-        Address firstAddress = new Address(3L, "Baker Street", "22A", "96047", null);
-        Address secondAddress = new Address(4L, "Pasadena", "4C", "96052", null);
-
-        this.addressRepo.saveAll(Arrays.asList(firstAddress, secondAddress));
-        User Dan = new User(3L, "DanTheMan", "Man123", LocalDate.of(1991, 10, 12),new ArrayList<>(Arrays.asList(secondAddress, firstAddress)));
-
-       
-
-        List<OrderItem> orderItems= new ArrayList<>();
+        cartItems = (List<String>) session.getAttribute("CartSession");
+        List<Bottle> bottleItems = new ArrayList<>();
+        List<Crate> crateItems = new ArrayList<>();
 
         for (String item:cartItems) {
-            orderItems.add(new Gson().fromJson(item,OrderItem.class));
-
+            bottleItems.add(new Gson().fromJson(item, Bottle.class));
+//            crateItems.add(new Gson().fromJson(item, Crate.class));
         }
-        Orders createOrders= new Orders();
-        createOrders.setPrice(100);
-        createOrders.setUser(Dan);
+//        model.addAttribute("cartSessionCrate",crateItems);
+        model.addAttribute("cartSession", bottleItems);
+        return "basket";
+    }
 
-        this.ordersrepo.save(createOrders);
+    @PostMapping
+    public String submitOrders(Model model, @RequestParam Long bev_id, @RequestParam String bev_quantity){
+//        Address firstAddress = new Address(3L, "Baker Street", "22A", "96047", null);
+//        Address secondAddress = new Address(4L, "Pasadena", "4C", "96052", null);
+//
+//        this.addressRepo.saveAll(Arrays.asList(firstAddress, secondAddress));
+//        User Dan = new User(3L, "DanTheMan", "Man123", LocalDate.of(1991, 10, 12),new ArrayList<>(Arrays.asList(secondAddress, firstAddress)));
 
-        for (OrderItem item:orderItems) {
-            item.setOrders(createOrders);
+       long id= bev_id;
+       String quantity = bev_quantity;
 
-        }
-
-
-
-        this.orderItemRepository.saveAll(orderItems);
+//        List<OrderItem> orderItems= new ArrayList<>();
+//
+//        for (String item:cartItems) {
+//            orderItems.add(new Gson().fromJson(item,OrderItem.class));
+//        }
+//        Orders createOrders= new Orders();
+//        createOrders.setPrice(100);
+//        createOrders.setUser(Dan);
+//
+//        this.ordersrepo.save(createOrders);
+//
+//        for (OrderItem item:orderItems) {
+//            item.setOrders(createOrders);
+//
+//        }
+//
+//
+//
+//        this.orderItemRepository.saveAll(orderItems);
 
         return "orders";
     }
